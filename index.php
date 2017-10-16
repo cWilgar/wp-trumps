7896fb62ebbca10d2ddf3748e35c9a2b3cd74877
cWilgar <carolinewilgar@gmail.com>
2017-10-16 16:51:31 +0100

    Add info index page

--- index.php
+++ index.php
@@ -15,3 +15,8 @@ define('WP_USE_THEMES', true);
 
 /** Loads the WordPress Environment and Template */
 require( dirname( __FILE__ ) . '/wp/wp-blog-header.php' );
+
+?>
+
+<h1>API only</h1>
+<p><a href="/wp-json/wp-trumps/v1/cards/">View Top-Trump Cards</a></p>
